596816624c53542761f94f04c3f87437339a4b35
Vitaliy <600merin.174@mail.ru>
2019-05-23 12:52:03 +0300

    Загружаю рабочее пространство

---
+++ src/index.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+
+    let calc = require(''),
+        form = require(''),
+        slider = require(''),
+        tabs = require(''),
+        timer = require(''),
+        modal = require('');
+    
+    calc();
+    form();
+    slider();
+    tabs();
+    timer();
+    modal();
+});
